c9ff0628e5fd1beb72ae1af7472585efa90d7c5b
Javier <jmartin@grupoloyal.com>
2022-11-09 13:21:33 +0100

    Repositorio Redis

--- catalogo/src/main/java/com/example/application/proxies/MeGustaProxy.java
+++ catalogo/src/main/java/com/example/application/proxies/MeGustaProxy.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name="MEGUSTA-SERVICE")
 public interface MeGustaProxy {
-	@PostMapping(path = "/me-gusta/{id}")
+	@PostMapping(path = "/me-gusta/hash/{id}")
 	String sendLike(@PathVariable int id);
-	@PostMapping(path = "/me-gusta/{id}")
+	@PostMapping(path = "/me-gusta/hash/{id}")
 	String sendLike(@PathVariable int id, @RequestHeader(value = "Authorization", required = true) String authorization);
 }
--- megusta/src/main/java/com/example/MeGustaService.java
+++ megusta/src/main/java/com/example/MeGustaService.java
@@ -39,44 +39,20 @@ public class MeGustaService {
 		hashOperations = template.opsForHash();
 	}
 
-	@GetMapping
+	@GetMapping("/cont")
 	@Operation(summary = "Informa de cuantos Me Gusta lleva actualmente")
 	private String get() {
 		return "Llevas " + redisValue.get(ME_GUSTA_CONT);
 	}
 
-	@GetMapping("/{id}")
-	@Operation(summary = "Informa de cuantos Me Gusta lleva actualmente una pelicula")
-	private String getPeli(@PathVariable int id) {
-		var count = hashOperations.get(ME_GUSTA_PELIS, Integer.toString(id));
-		return "Llevas " + (count == null ? "0" : count) + " likes";
-	}
-
-	@PostMapping
+	@PostMapping("/cont")
 	@Operation(summary = "Manda un Me Gusta")
 	@SecurityRequirement(name = "bearerAuth")
 	private String add() {
 		return "Llevas " + redisValue.increment(ME_GUSTA_CONT);
 	}
 
-	@PostMapping("/{id}")
-	@Operation(summary = "Manda un Me Gusta a una pelicula")
-	@SecurityRequirement(name = "bearerAuth")
-	private String addPeli(@PathVariable int id) {
-		return "Llevas " + hashOperations.increment(ME_GUSTA_PELIS, Integer.toString(id), 1);
-	}
-	@PutMapping("/{miles}/miles")
-	@Operation(summary = "Manda miles de Me Gusta a las pelicula")
-	@SecurityRequirement(name = "bearerAuth")
-	private String addPeliMiles(@PathVariable int miles) {
-		Random rnd = new Random();
-		Date ini = new Date();
-		for (int i = 0; i++ < miles * 1000; hashOperations.increment(ME_GUSTA_PELIS, Integer.toString(rnd.nextInt(1, 201)), 1))
-			;
-		return "Ha tardado " + ((new Date()).getTime() - ini.getTime()) + " ms.";
-	}
-
-	@PutMapping("/{miles}")
+	@PutMapping("/cont/{miles}")
 	@Operation(summary = "Manda miles de Me Gusta")
 	private String add(@Parameter(description = "Número de miles a enviar") @PathVariable int miles) {
 		long r = 0;
@@ -84,6 +60,67 @@ public class MeGustaService {
 		for (int i = 0; i++ < miles * 1000; r = redisValue.increment(ME_GUSTA_CONT))
 			;
 		return "Llevas " + r + ". Ha tardado " + ((new Date()).getTime() - ini.getTime()) + " ms.";
+	}
+
+	@GetMapping("/hash/{id}")
+	@Operation(summary = "Informa de cuantos Me Gusta lleva actualmente una pelicula")
+	private String getPeli(@PathVariable int id) {
+		var count = hashOperations.get(ME_GUSTA_PELIS, Integer.toString(id));
+		return "Llevas " + (count == null ? "0" : count) + " likes";
+	}
+
+	@PostMapping("/hash/{id}")
+	@Operation(summary = "Manda un Me Gusta a una pelicula")
+	@SecurityRequirement(name = "bearerAuth")
+	private String addPeli(@PathVariable int id) {
+		return "Llevas " + hashOperations.increment(ME_GUSTA_PELIS, Integer.toString(id), 1);
+	}
+
+	@PutMapping("/hash/{miles}")
+	@Operation(summary = "Manda miles de Me Gusta a las pelicula")
+	@SecurityRequirement(name = "bearerAuth")
+	private String addPeliMiles(@PathVariable int miles) {
+		Random rnd = new Random();
+		Date ini = new Date();
+		for (int i = 0; i++ < miles * 1000; hashOperations.increment(ME_GUSTA_PELIS, Integer.toString(rnd.nextInt(1, 1001)), 1))
+			;
+		return "Ha tardado " + ((new Date()).getTime() - ini.getTime()) + " ms.";
+	}
+	
+	@Autowired
+	PeliculaContRepository dao;
+	
+	@GetMapping("/repo/{id}")
+	@Operation(summary = "Informa de cuantos Me Gusta lleva actualmente una pelicula")
+	private String getRepo(@PathVariable int id) {
+		var item = dao.findById(id);
+		return "Llevas " + (item.isEmpty() ? "0" : item.get().getCont()) + " likes";
+	}
+
+	@PostMapping("/repo/{id}")
+	@Operation(summary = "Manda un Me Gusta a una pelicula")
+	@SecurityRequirement(name = "bearerAuth")
+	private String addRepo(@PathVariable int id) {
+		var item = dao.findById(id);
+		PeliculaCont peli;
+		if(item.isEmpty()) {
+			peli =new PeliculaCont(id, 1);
+		} else {
+			peli = item.get();
+			peli.addCont();
+		}
+		dao.save(peli);
+		return "Llevas " + peli.getCont();
+	}
+
+	@PutMapping("/repo/{miles}")
+	@Operation(summary = "Manda miles de Me Gusta a las pelicula")
+	@SecurityRequirement(name = "bearerAuth")
+	private String addRepoMiles(@PathVariable int miles) {
+		Random rnd = new Random();
+		Date ini = new Date();
+		for (int i = 0; i++ < miles * 1000; addRepo(rnd.nextInt(1, 101)));
+		return "Ha tardado " + ((new Date()).getTime() - ini.getTime()) + " ms.";
 	}
 
 }
